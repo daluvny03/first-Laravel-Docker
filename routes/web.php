e06a948bf599d6ae764d15d870468a81f2161609
daluvny03 <daluvny03@gmail.com>
2026-09-11 22:42:12 +0700

    feat: add navbar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'halo dari Laravel Docker, Laravel _ Docker sudah berhasil, HORE!';
 });
+
+Route::get('/navbar', function() {
+    return 'Ini halaman Navbar Developer B';
+});
